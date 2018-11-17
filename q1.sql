6d8cfa69f86efb1d01f0ff243befff6aeaefef69
tautaus <43488851+tautaus@users.noreply.github.com>
2018-11-17 15:44:58 -0500

    Add files via upload

--- q1.sql
+++ q1.sql
@@ -14,14 +14,14 @@ partyName VARCHAR(100)
 
 
 -- You may find it convenient to do this for each of the views
-create view as zeroToFive(
-    SELECT year, countryName, "(0,5]" as voteRange, partyName
+create VIEW zeroToFive AS
+    SELECT date_part('year',e_date), countryName, "(0,5]" as voteRange, partyName
     FROM ((party join election_result on party.id = election_result.party_id) 
                 join election on election_result.election_id = election.id)
                 join country on country.id = election.country_id
     WHERE election_result.votes/election.votes_valid > 0 and 
         election_result.votes/election.votes_valid <= 5
-);
+
 -- that define your intermediate steps.  (But give them better names!)
 -- DROP VIEW IF EXISTS intermediate_step CASCADE;
 
