023b6600b468c2500291c526fa2ebe5798575c53
tautaus <43488851+tautaus@users.noreply.github.com>
2018-11-17 23:32:27 -0500

    Add files via upload

--- q4.sql
+++ q4.sql
@@ -23,42 +23,42 @@ from (country join party on country.id = party.country_id) join party_position o
 create view count1 AS
 select name, count(*) as c1
 from infor
+where left_right >= 0 and left_right < 2
 group by name
-having left_right >= 0 and left_right < 2
 ;
 
 create view count2 AS
 select name, count(*) as c2
 from infor
+where left_right >= 2 and left_right < 4
 group by name
-having left_right >= 2 and left_right < 4
 ;
 
 create view count3 AS
 select name, count(*) as c3
 from infor
+where left_right >= 4 and left_right < 6
 group by name
-having left_right >= 4 and left_right < 6
 ;
 
 create view count4 AS
 select name, count(*) as c4
 from infor
+where left_right >= 6 and left_right < 8
 group by name
-having left_right >= 6 and left_right < 8
 ;
 
 create view count5 AS
 select name, count(*) as c5
 from infor
+where left_right >= 8 and left_right < 10
 group by name
-having left_right >= 8 and left_right < 10
 ;
 
 create view answer AS
 select count1.name, c1, c2, c3, c4, c5
 from count1, count2, count3, count4, count5
-where count1.name = count2.name and count2.name = count3.name and count3.name = count4.name and count4.name = count5.name
+where count1.name = count2.name, count2.name = count3.name, count3.name = count4.name , count4.name = count5.name
 ;
 
 -- You may find it convenient to do this for each of the views
