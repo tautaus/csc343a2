7bba66c53684c27ab6eecf91676013fe63ca1456
tautaus <43488851+tautaus@users.noreply.github.com>
2018-11-17 23:21:00 -0500

    Add files via upload

--- q4.sql
+++ q4.sql
@@ -15,13 +15,57 @@ CREATE TABLE q4(
         r8_10 INT
 );
 
+create view infor AS
+select country.name, party_position.party_id, left_right
+from (country join party on country.id = party.country_id) join party_position on party_position.party_id = party.id
+;
+
+create view count1 AS
+select name, count(*) as c1
+group by name
+having left_right >= 0 and left_right < 2
+;
+
+create view count2 AS
+select name, count(*) as c2
+group by name
+having left_right >= 2 and left_right < 4
+;
+
+create view count3 AS
+select name, count(*) as c3
+group by name
+having left_right >= 4 and left_right < 6
+;
+
+create view count4 AS
+select name, count(*) as c4
+group by name
+having left_right >= 6 and left_right < 8
+;
+
+create view count5 AS
+select name, count(*) as c5
+group by name
+having left_right >= 8 and left_right < 10
+;
+
+create view answer AS
+select count1.name, c1, c2, c3, c4, c5
+from count1, count2, count3, count4, count5
+where count1.name = count2.name, count2.name = count3.name, count3.name = count4.name , count4.name = count5.name
+;
+
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
 
 
 -- the answer to the query 
 INSERT INTO q4 
+SELECT name,c1,c2,c3,c4,c5
+from answer
+;
 
+DROP VIEW IF EXISTS answer CASCADE;
