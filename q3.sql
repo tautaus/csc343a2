185536e1584689882e8b4a606aeee7e1f58d84dc
tautaus <43488851+tautaus@users.noreply.github.com>
2018-11-17 22:59:28 -0500

    Add files via upload

--- q3.sql
+++ q3.sql
@@ -11,6 +11,28 @@ create table q3(
         participationRatio real
 );
 
+create view ratio as 
+select country_id, year, sum(votes_cast)/sum(electorate) as ratio
+from election
+group by (country_id, date_part('year',e_date) as year)
+;
+
+create view ct as
+select DISTINCT(r1.country_id)
+from ratio r1, ratio r2
+where r1.country_id = r2.country_id and r1.year < r2.year and r1.ratio <= r2.ratio and r1.year >= 2001 and r1.year <= 2016
+;
+
+create view ctt as
+select ct.country_id, country.name
+from ct join country on ct.country_id = country.id
+;
+
+create view answer as
+select name,year,ratio
+from ctt join ratio on ctt.country_id = ratio.country_id
+;
+
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
 DROP VIEW IF EXISTS intermediate_step CASCADE;
@@ -19,5 +41,7 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 
 -- the answer to the query 
-insert into q3 
-
+insert into q3(countryName,year,participationRatio) 
+select name,year,ratio
+from answer
+;
